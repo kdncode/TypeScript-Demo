e75f6e77cd71c2d693ed49515a56075a97fed099
kdncode <kdncode@gmail.com>
2018-05-17 23:09:53 -0500

    updated

--- greeter.js
+++ greeter.js
@@ -1,5 +1,8 @@
+// function greeter(person) {
+//     return "Hello, " + person;
+// }
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = "John";
+var user = { firstName: "John", lastName: "Wick" };
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,20 @@
-function greeter(person) {
-    return "Hello, " + person;
+// function greeter(person) {
+//     return "Hello, " + person;
+// }
+
+// let user = "John Wick";
+// document.body.innerHTML = greeter(user);
+
+//Interface
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = "John";
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "John", lastName: "Wick" };
+
 document.body.innerHTML = greeter(user);
