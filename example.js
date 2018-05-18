fed5d40ddd27cadcb75de3274663809db19e428f
kdncode <kdncode@gmail.com>
2018-05-17 23:37:04 -0500

    updated

--- example.js
+++ example.js
@@ -7,3 +7,8 @@ students = ['John', 'Joanna', 'Teo'];
 for (var i = 0; i < students.length; i++) {
     console.log(students[i]);
 }
+/* Number */
+var myNumber = [2, 3, 5];
+for (var i = 0; i < myNumber.length; i++) {
+    console.log(myNumber[i]);
+}
--- example.ts
+++ example.ts
@@ -10,3 +10,9 @@ students = ['John', 'Joanna', 'Teo']
 for (let i = 0; i < students.length; i++ ) {
     console.log(students[i])
 }
+
+/* Number */
+let myNumber : number[] = [2, 3, 5];
+for ( let i = 0; i < myNumber.length; i++ ) {
+    console.log(myNumber[i])
+}
