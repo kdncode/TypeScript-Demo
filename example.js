e8fbac0f87c6faf6f7d010c7c377702e7f9ad4ab
kdncode <kdncode@gmail.com>
2018-05-17 23:35:20 -0500

    updated

---
+++ example.js
@@ -0,0 +1,9 @@
+var a;
+a = 5;
+console.log(a);
+/* Array */
+var students;
+students = ['John', 'Joanna', 'Teo'];
+for (var i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
---
+++ example.ts
@@ -0,0 +1,12 @@
+let a: number;
+a = 5;
+
+console.log(a)
+
+/* Array */
+let students: string[];
+students = ['John', 'Joanna', 'Teo'] 
+
+for (let i = 0; i < students.length; i++ ) {
+    console.log(students[i])
+}
