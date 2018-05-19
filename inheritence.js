8438816053f0f2752dfeeeeb9efcf006169a423a
kdncode <kdncode@gmail.com>
2018-05-18 22:58:33 -0500

    added Void Super.Function

--- inheritence.js
+++ inheritence.js
@@ -1,3 +1,8 @@
+// // Create a class
+// class Cars {
+//     name: string;
+//     price: number;
+//     color: string [];
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -8,6 +13,47 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+//     constructor(name: string, price: number, color: string[]) {
+//         this.name = name;
+//         this.price = price;
+//         this.color = color;
+//     }
+//     showInfo() {
+//         let myInfo : string = '';
+//         for (let i = 0; i < this.color.length; i++) {
+//             myInfo += this.color[i];
+//             myInfo += ', '
+//         }
+//         return `Car name: ${this.name}
+//                 Price: ${this.price}
+//                 color: ${myInfo}`
+//     }
+// }
+// let honda = new Cars('Honda', 30000, ['green', 'red', 'black', 'white'])
+// let toyota = new Cars('Toyota', 20000, ['gold', 'orange', 'gray'])
+// console.log(honda.showInfo());
+// console.log(toyota.showInfo());
+// Inheritence 
+// class Moto extends Cars {
+//     tire: number;
+//     constructor(name: string, price: number, color: string[], tire: number) {
+//         super(name, price, color);
+//         this.tire = tire;
+//     }
+//     showInfo() : string {
+//         let myInfo : string = '';
+//         for (let i = 0; i < this.color.length; i++) {
+//             myInfo += this.color[i];
+//             myInfo += ', '
+//         }
+//         return `Car name: ${this.name}
+//                 Price: ${this.price}
+//                 color: ${myInfo}
+//                 tire: ${this.tire}`
+//     }
+// }
+// let suzuki = new Moto('Suzuki', 3000, ['green', 'rose'], 2);
+// console.log(suzuki.showInfo());
 // Create a class
 var Cars = /** @class */ (function () {
     function Cars(name, price, color) {
@@ -21,15 +67,14 @@ var Cars = /** @class */ (function () {
             myInfo += this.color[i];
             myInfo += ', ';
         }
-        return "Car name: " + this.name + "\n                Price: " + this.price + "\n                color: " + myInfo;
+        var myCar = "Car name: " + this.name + "\n                Price: " + this.price + "\n                color: " + myInfo;
+        console.log(myCar);
     };
     return Cars;
 }());
 var honda = new Cars('Honda', 30000, ['green', 'red', 'black', 'white']);
-var toyota = new Cars('Toyota', 20000, ['gold', 'orange', 'gray']);
-console.log(honda.showInfo());
-console.log(toyota.showInfo());
-// Inheritence 
+honda.showInfo();
+// Shorten the code in function by using super.function
 var Moto = /** @class */ (function (_super) {
     __extends(Moto, _super);
     function Moto(name, price, color, tire) {
@@ -38,14 +83,12 @@ var Moto = /** @class */ (function (_super) {
         return _this;
     }
     Moto.prototype.showInfo = function () {
-        var myInfo = '';
-        for (var i = 0; i < this.color.length; i++) {
-            myInfo += this.color[i];
-            myInfo += ', ';
-        }
-        return "Car name: " + this.name + "\n                Price: " + this.price + "\n                color: " + myInfo + "\n                tire: " + this.tire;
+        _super.prototype.showInfo.call(this);
+        console.log("With " + this.tire + " tires");
     };
     return Moto;
 }(Cars));
 var suzuki = new Moto('Suzuki', 3000, ['green', 'rose'], 2);
-console.log(suzuki.showInfo());
+var yamaha = new Moto('Yamaha', 4000, ['black', 'red'], 2);
+suzuki.showInfo();
+yamaha.showInfo();
--- inheritence.ts
+++ inheritence.ts
@@ -1,3 +1,63 @@
+// // Create a class
+// class Cars {
+//     name: string;
+//     price: number;
+//     color: string [];
+
+//     constructor(name: string, price: number, color: string[]) {
+//         this.name = name;
+//         this.price = price;
+//         this.color = color;
+//     }
+
+//     showInfo() {
+//         let myInfo : string = '';
+
+//         for (let i = 0; i < this.color.length; i++) {
+//             myInfo += this.color[i];
+//             myInfo += ', '
+//         }
+
+//         return `Car name: ${this.name}
+//                 Price: ${this.price}
+//                 color: ${myInfo}`
+//     }
+// }
+
+// let honda = new Cars('Honda', 30000, ['green', 'red', 'black', 'white'])
+// let toyota = new Cars('Toyota', 20000, ['gold', 'orange', 'gray'])
+
+// console.log(honda.showInfo());
+// console.log(toyota.showInfo());
+
+
+// Inheritence 
+// class Moto extends Cars {
+//     tire: number;
+
+//     constructor(name: string, price: number, color: string[], tire: number) {
+//         super(name, price, color);
+//         this.tire = tire;
+//     }
+
+//     showInfo() : string {
+//         let myInfo : string = '';
+
+//         for (let i = 0; i < this.color.length; i++) {
+//             myInfo += this.color[i];
+//             myInfo += ', '
+//         }
+
+//         return `Car name: ${this.name}
+//                 Price: ${this.price}
+//                 color: ${myInfo}
+//                 tire: ${this.tire}`
+//     }
+// }
+
+// let suzuki = new Moto('Suzuki', 3000, ['green', 'rose'], 2);
+// console.log(suzuki.showInfo());
+
 // Create a class
 class Cars {
     name: string;
@@ -18,20 +78,21 @@ class Cars {
             myInfo += ', '
         }
 
-        return `Car name: ${this.name}
+        let myCar = `Car name: ${this.name}
                 Price: ${this.price}
-                color: ${myInfo}`
+                color: ${myInfo}`;
+        
+        console.log(myCar);     
     }
 }
 
 let honda = new Cars('Honda', 30000, ['green', 'red', 'black', 'white'])
-let toyota = new Cars('Toyota', 20000, ['gold', 'orange', 'gray'])
 
-console.log(honda.showInfo());
-console.log(toyota.showInfo());
+honda.showInfo();
 
 
-// Inheritence 
+// Shorten the code in function by using super.function
+
 class Moto extends Cars {
     tire: number;
 
@@ -40,20 +101,14 @@ class Moto extends Cars {
         this.tire = tire;
     }
 
-    showInfo() : string {
-        let myInfo : string = '';
-
-        for (let i = 0; i < this.color.length; i++) {
-            myInfo += this.color[i];
-            myInfo += ', '
-        }
-
-        return `Car name: ${this.name}
-                Price: ${this.price}
-                color: ${myInfo}
-                tire: ${this.tire}`
+    showInfo() : void {
+        super.showInfo();
+        console.log(`With ${this.tire} tires`);
+        
     }
 }
 
 let suzuki = new Moto('Suzuki', 3000, ['green', 'rose'], 2);
-console.log(suzuki.showInfo());
+let yamaha = new Moto('Yamaha', 4000, ['black', 'red'], 2);
+suzuki.showInfo();
+yamaha.showInfo();
