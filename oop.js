f67fc7cf98bd24e2a2ccfb770e069aa6dfefb39e
kdncode <kdncode@gmail.com>
2018-05-18 21:08:08 -0500

    created a real world class

--- oop.js
+++ oop.js
@@ -15,3 +15,18 @@ var character1 = new gameCharacters('Darknight', 22);
 var character2 = new gameCharacters('Silverguy', 44);
 console.log(character1.show());
 console.log(character2.show());
+// Create a phone class
+var phones = /** @class */ (function () {
+    function phones(name, price, rate, color) {
+        this.name = name;
+        this.price = price;
+        this.rate = rate;
+        this.color = color;
+    }
+    phones.prototype.showInfo = function () {
+        return "Your phone: " + this.name + " : price: " + this.price + "\n                rating: " + this.rate + " stars & color: " + this.color.length + " colors";
+    };
+    return phones;
+}());
+var phone1 = new phones('iPhone X', 1000, 4, ['silver', 'gold', 'red']);
+console.log(phone1.showInfo());
--- oop.ts
+++ oop.ts
@@ -20,5 +20,28 @@ var character1 = new gameCharacters('Darknight', 22);
 var character2 = new gameCharacters('Silverguy', 44);
 
 console.log(character1.show());
-
 console.log(character2.show());
+
+// Create a phone class
+class phones {
+    name: string;
+    price: number;
+    rate: number;
+    color: [];
+
+    constructor(name: string, price: number, rate: number, color: string[]) {
+        this.name = name;
+        this.price = price;
+        this.rate = rate;
+        this.color = color;
+    }
+
+    showInfo() {
+        return `Your phone: ${this.name} : price: ${this.price}
+                rating: ${this.rate} stars & color: ${this.color.length} colors`
+    }
+}
+
+var phone1 = new phones('iPhone X', 1000, 4, ['silver', 'gold', 'red'])
+
+console.log(phone1.showInfo());
