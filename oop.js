625c983d2bb4d6c73b21d3a881c2fead37537e67
kdncode <kdncode@gmail.com>
2018-05-18 21:00:45 -0500

    updated

--- oop.js
+++ oop.js
@@ -12,4 +12,6 @@ var gameCharacters = /** @class */ (function () {
     return gameCharacters;
 }());
 var character1 = new gameCharacters('Darknight', 22);
+var character2 = new gameCharacters('Silverguy', 44);
 console.log(character1.show());
+console.log(character2.show());
--- oop.ts
+++ oop.ts
@@ -17,4 +17,8 @@ class gameCharacters {
 }
 
 var character1 = new gameCharacters('Darknight', 22);
+var character2 = new gameCharacters('Silverguy', 44);
+
 console.log(character1.show());
+
+console.log(character2.show());
